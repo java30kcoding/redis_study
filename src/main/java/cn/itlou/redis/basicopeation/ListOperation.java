8ce77114c7120a6c1567fea8371d7e07b98f2803
yuanyulou <434356430@qq.com>
2019-10-11 13:45:31 +0800

    List操作不全

--- src/main/java/cn/itlou/redis/basicopeation/ListOperation.java
+++ src/main/java/cn/itlou/redis/basicopeation/ListOperation.java
@@ -3,6 +3,8 @@ package cn.itlou.redis.basicopeation;
 import lombok.extern.slf4j.Slf4j;
 import redis.clients.jedis.Jedis;
 
+import java.util.List;
+
 /**
  * List的操作
  *
@@ -22,7 +24,8 @@ public class ListOperation {
 
     public static void main(String[] args) {
 //        testListLikeQueue();
-        testListLikeStack();
+//        testListLikeStack();
+        testListSlowOperation();
     }
 
     /**
@@ -65,9 +68,34 @@ public class ListOperation {
      * list的慢操作
      */
     private static void testListSlowOperation(){
-
-
-
+        String listName = "list02";
+        jedis.del(listName);
+        jedis.rpush(listName, "java", "scala", "go");
+        Long llen = jedis.llen(listName);
+        log.info("list02的长度: {}", llen);
+        //注意为 O(n)的操作
+        String lindexResult = jedis.lindex(listName, 1);
+        log.info("> lindex list02 1");
+        log.info("scala");
+        log.info("lindexResult: {}", lindexResult);
+        //遍历list；O(n)
+        List<String> lrangeResult = jedis.lrange(listName, 0, -1);
+        log.info("> lrange list02 0 -1");
+        log.info("1) \"java\"\n" +
+                 "2) \"scala\"\n" +
+                 "3) \"go\"");
+        log.info("lrangeResult: {}", lrangeResult);
+        //ltrim截取
+        String ltrimResult = jedis.ltrim(listName, 1, -1);
+        log.info("> ltrim list02 1 -1");
+        log.info("OK");
+        log.info("ltrimResult: {}", ltrimResult);
+        List<String> afterTrim = jedis.lrange(listName, 0, -1);
+        log.info("ltrim后的数据: {}", afterTrim);
+        //ltrim 1 0 是清空整个列表
+        jedis.ltrim(listName, 1, 0);
+        List<String> afterTrimAllList = jedis.lrange(listName, 0, -1);
+        log.info("ltrim所有列表后的数据: {}", afterTrimAllList);
     }
 
 }
